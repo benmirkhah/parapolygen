7c60212b7f92a923ef1341f351ecae3520f5f6d8
Ben Mirkhah <benmirkhah@users.noreply.github.com>
2025-12-31 05:53:11 -0700

    Initial commit

---
+++ esppg/src/classes/ppg-element-class.mjs
@@ -0,0 +1,15 @@
+//Generic element template to build other elements from------------------------
+export class Element {
+  static ecount = 0; //Number of instances
+
+  constructor ({
+    id    =  '',
+    cname =  '',
+    html  =  '',
+  } = {}){
+    this.id    =    id;
+    this.cname = cname;
+    this.html  =  html;
+    Element.ecount++;
+  }
+}//----------------------------------------------------------------------------
---
+++ esppg/src/classes/ppg-group-class.mjs
@@ -0,0 +1,45 @@
+import { Element } from "./ppg-element-class.mjs";
+import { makeid } from "../helpers/ppg-random.mjs";
+
+//A group of one or more shapes------------------------------------------------
+class Group extends Element{
+  static gcount =  1;
+  zindex  =        1;
+  members = Object();
+  //------------------------------------------------------------
+  constructor(id = 'G'+makeid(), members = {}, cname = '') {
+    super({'id':id, 'cname':cname });
+    this.zindex  = Group.gcount++;
+    if (members.length) { 
+      this.members = { ...members };
+    }
+  }
+  //------------------------------------------------------------
+  add(id = 0) {
+    if(id) { this.members[id] = id; }
+    return id;
+  }
+  //------------------------------------------------------------
+  remove(id = 0) {
+    if(Object.hasOwn(this.members, id)) {
+      delete this.members[id]; 
+    }
+    return id;
+  }
+  //------------------------------------------------------------
+  render() {
+    let out = '<g ';
+    out += 'id="'+this.id+'"'+(this.cname ? ' class="'+this.cname+'" ' : ' ');
+    out += 'style="zindex: '+this.zindex+'">\r\n';
+
+    Object.keys(this.members).forEach(shapeID => {
+      //shape = shapes[shapeID];
+      //out  += '  '+shape.render();
+      out  += '   <!-- '+shapeID+' goes here -->\r\n';
+    });
+    out += '</g>\r\n';
+    return out;
+  }
+}//----------------------------------------------------------------------------
+
+export default Group;
