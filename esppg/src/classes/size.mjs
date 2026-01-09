1b451cf6b529b8994bd1fb4426c10f3921dabbc4
Ben Mirkhah <benmirkhah@users.noreply.github.com>
2026-01-09 05:52:44 -0700

    Size revamped

--- esppg/src/classes/size.mjs
+++ esppg/src/classes/size.mjs
@@ -1,22 +1,35 @@
-//Any width, height, or radius----------------------------------------------
+//Size based on width/height, or radius----------------------------------------
 class Size {
   constructor({
-    w  =  0,
-    h  =  0,
-    r  =  0,
+    w = 0, //Width
+    h = 0, //Height
+    r = 0, //Radius (inner)
+    o = 0, //Outer radius
   } = {}) {
-    this.w  =  w;
-    this.h  =  h;
-    this.r  =  r;
+    //Keep arg > 0
+    if(w) this.w = w;
+    if(h) this.h = h;
+    if(r) this.r = r;
+    if(o) this.o = o;
   } 
   //------------------------------------------------------------
   log() {
     let out = 'l[ ';
     out += this.w  ?  'w:'+this.w +' ' : '';
     out += this.h  ?  'h:'+this.h +' ' : '';
-    out += this.r  ?  't:'+this.r +' ' : '';
+    out += this.r  ?  'r:'+this.r +' ' : '';
+    out += this.o  ?  'o:'+this.o +' ' : '';
     out += ']';
     console.log(out);
+    return out;
+  }
+  //------------------------------------------------------------
+  duplicate() {
+    let out = new Object(null);
+    if(typeof(this.w) == 'number') out['w'] = this.w;
+    if(typeof(this.h) == 'number') out['h'] = this.h;
+    if(typeof(this.r) == 'number') out['r'] = this.r;
+    if(typeof(this.o) == 'number') out['o'] = this.o;
     return out;
   }
 }//----------------------------------------------------------------------------
