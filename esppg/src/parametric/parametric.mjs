ccd216c44945d21662288392f428c9c6cdcd2e97
Ben Mirkhah <benmirkhah@users.noreply.github.com>
2026-01-11 11:20:27 -0700

    Toggle() added

--- esppg/src/parametric/parametric.mjs
+++ esppg/src/parametric/parametric.mjs
@@ -1,18 +1,35 @@
-//Template class for all the parametric functions to extend--------------------
+//Template class for all parametric iterator functions to extend---------------
 class Parametric {
-  static count = 0;
+  static total = 0;
+  static onnC  = 0;  //Count number of on
+  static offC  = 0;  //Count number of off
 
   constructor({ 
     kind  = 'fixed', 
-    value =       0,
+    value = 0,
+    param = 0, //Value of delta, scaler, etc. 
+    every = 0, //Stay on number (use value)
+    altv  = 0, //Alternate value
+    skip  = 0, //Stay off number (use altv)
+    done  = 0, //Max iteration number
+    pname = '' //Name of delta, scaler, etc.
   } = {}) {
-    this.kind = kind;
-    this.val  = value;
-    Parametric.count++;
+    this.kind  = kind;
+    this.value = value;
+    if(param) this.start = value; //Initial value
+    if(param) this.param = param;
+    if(pname) this.pname = pname;
+    if(every) this.every = every;
+    if(altv)  this.altv  =  altv;
+    if(skip)  this.skip  =  skip;
+    if(done)  this.done  =  done;
+
+    Parametric.total++;
   }
   //------------------------------------------------------------
-  count() { return Parametric.count; }
+  total() { return Parametric.total; }
   //------------------------------------------------------------
+  val()   { return this.value; }
 } 
 //-----------------------------------------------------------------------------
 
