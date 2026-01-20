6d8660932176ec67c8a2b2710b537884d137b2dd
Ben Mirkhah <benmirkhah@users.noreply.github.com>
2026-01-20 13:56:56 -0700

    Position class updated

--- esppg/src/classes/position.mjs
+++ esppg/src/classes/position.mjs
@@ -1,4 +1,5 @@
-import Point from "./point.mjs";
+import { isObj } from "../utils/typeutils.mjs";
+import Point     from "./point.mjs";
 
 //Position parameters of each shape--------------------------------------------
 class Position {
@@ -6,14 +7,45 @@ class Position {
     this.x =  x;
     this.y =  y;
     if (kind) this.kind = kind;
-    if (kind == 'center') this.center = new Point({id:'center'});
-    if (kind == 'tlc'   ) this.tlc    = new Point({id:'tlc'});
-    if (kind == 'trc'   ) this.trc    = new Point({id:'trc'});
-    if (kind == 'blc'   ) this.blc    = new Point({id:'blc'});
-    if (kind == 'brc'   ) this.brc    = new Point({id:'brc'});
+    switch (kind) {
+      case 'center': this.center = new Point({id:'center', x:x, y:y }); break;
+      case 'tlc'   : this.tlc    = new Point({id:'tlc'   , x:x, y:y }); break;
+      case 'trc'   : this.trc    = new Point({id:'trc'   , x:x, y:y }); break;
+      case 'blc'   : this.blc    = new Point({id:'blc'   , x:x, y:y }); break;
+      case 'brc'   : this.brc    = new Point({id:'brc'   , x:x, y:y }); break;
+    }
   }
   //------------------------------------------------------------
-  //total() { return Position.total; }
+  render() {
+    let out = '';
+    out += isObj(this.center) ? this.center.render(): '' ;
+    out += isObj(this.tlc)    ? this.tlc.render()   : '' ;
+    out += isObj(this.trc)    ? this.trc.render()   : '' ;
+    out += isObj(this.blc)    ? this.blc.render()   : '' ;
+    out += isObj(this.brc)    ? this.brc.render()   : '' ;
+    return out;
+  }
+  //------------------------------------------------------------
+  log() {
+    let out = '';
+    out += isObj(this.center) ? this.center.log(): '' ;
+    out += isObj(this.tlc)    ? this.tlc.log()   : '' ;
+    out += isObj(this.trc)    ? this.trc.log()   : '' ;
+    out += isObj(this.blc)    ? this.blc.log()   : '' ;
+    out += isObj(this.brc)    ? this.brc.log()   : '' ;
+    return out;
+  }
+  //------------------------------------------------------------
+  next() {
+    if (isObj(this.center)) this.center.next();
+    if (isObj(this.tlc)   ) this.tlc.next()   ;
+    if (isObj(this.trc)   ) this.trc.next()   ;
+    if (isObj(this.blc)   ) this.blc.next()   ;
+    if (isObj(this.brc)   ) this.brc.next()   ;
+    if (isObj(this.x  )   ) this.x.next()     ;
+    if (isObj(this.y  )   ) this.y.next()     ;
+    return {   value: this, done: false      };
+  }
 }//----------------------------------------------------------------------------
 
 export default Position;
