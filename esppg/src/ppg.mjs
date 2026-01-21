d298d8e00fddf6d5294f55bc7e57043b1c6d5bb1
Ben Mirkhah <benmirkhah@users.noreply.github.com>
2026-01-21 16:40:06 -0700

    Radial functions added

--- esppg/src/ppg.mjs
+++ esppg/src/ppg.mjs
@@ -1,4 +1,4 @@
-const VERSION = '0.046'; //Commits + 1
+const VERSION = '0.047'; //Commits + 1
 import G      from './utils/globals.mjs';
 import svgTag from './svgtags/svg.mjs';
 import config from './config/config.mjs';
---
+++ esppg/src/utils/radial.mjs
@@ -0,0 +1,97 @@
+import G        from "./globals.mjs";
+import roundInt from "./randoms.mjs";
+import Point    from "../classes/point.mjs";
+//--------------------------------------------------------------
+const radialOptions = {
+  x      :        G.MID.x, //center
+  y      :        G.MID.y, //center
+  o      :  G.MID.y - 100, //outer-most ring radius
+  r      :            100, //inner-most ring radius
+  a      :              0, //Starting angle
+  adelta :             15, //Angle  incremental delta
+  rdelta :            100, //Radius incremental delta
+  rings  :              2, //Number of rings
+  jewels :             12, //Jewels per ring
+  bound  :              0, //Ignore points outside bounding box?
+}//-------------------------------------------------------------
+
+//Radial Rings & Jewel grid (calculate R/A delta based on Rings & Jewels)------
+export function radialRJ(options = radialOptions) {
+  let r      = options.r;
+  let o      = options.o;
+  let rings  = options.rings; 
+  let jewels = options.jewels;
+  let dr     = Math.trunc((o-r) /  rings); //Radius delta in pixels 
+  let da     = Math.trunc( 360  / jewels); //Angle delta in degrees
+  options.rdelta = dr;
+  options.adelta = da;
+  return radialGrid(options);
+}//----------------------------------------------------------------------------
+
+//Radial Radius & Angle grid (calculate Rings & Jewels based on R/A delta)-----
+export function radialRA(options = radialOptions) {
+  let r      = options.r;
+  let o      = options.o;
+  let dr     = options.rdelta;
+  let da     = options.adelta;
+  let rings  = Math.trunc((o-r) / dr); //Number of rings
+  let jewels = Math.trunc( 360  / da); //Jewels per ring
+
+  options.rings  = rings;
+  options.jewels = jewels;
+  return radialGrid(options);
+}//----------------------------------------------------------------------------
+
+//Returns an array of grid points for the requsted options--------------------- 
+function radialGrid(options = radialOptions) {
+  let cx     = options.x;
+  let cy     = options.y;
+  let r      = options.r;
+  let o      = options.o;
+  let a      = options.a;
+  let dr     = options.rdelta;
+  let da     = options.adelta;
+  let bound  = options.bound;
+  let rings  = options.rings;
+  let jewels = options.jewels;
+  let points = [ { z:zero, cx , cy, r, a } ]; //index 0 is the center mark 
+  let nudge  = (2 * Math.PI)/jewels; //360 degrees is (2 * Pi) in radians
+  let ignore = 0; //Number of ignored points
+  let radius = 0; //Radius of each ring
+  let radian = 0; //Angle in radian
+  let angle  = 0; //Angle in degrees
+  let msg    = '';
+  let num    = 1;
+  let x      = 0;
+  let y      = 0;
+
+  //Radial Clockwise
+  for (let ring=1; ring<=rings; ring++) {
+    for(let jewel=0; jewel<jewels; jewel++) {
+      radius = r + (ring * dr); //Inner ring + each delta
+      radian = nudge * (jewel + 0.001); //Fixes rounding error
+      angle  = roundInt(radian*(180/Math.PI),1)
+      x = roundInt(cx + (radius * Math.cos(radian)),1);
+      y = roundInt(cy + (radius * Math.sin(radian)),1);
+      if (bound && ((x<0) || (x>G.WIDTH) || (y<0) || (y>G.HEIGHT))) {
+        msg += ((x<0) || (x>G.WIDTH))  ? 'Ignored out of bounds (X: '+x+') ':'Even with a normal    (X: '+x+') ';  
+        msg += ((y<0) || (y>G.HEIGHT)) ? 'Ignored out of bounds (Y: '+y+') ':'Even with a normal    (Y: '+y+') '; 
+        msg += '\r\n';
+        ignore++;        
+      } else {
+        points[num] = { x:x , y:y, r:radius, a:(jewel*da) }; //skip index 0
+        num++;
+      }
+    }
+  }
+
+  if (G.DEBUG) {
+    console.log(points);
+    if (bound) {
+      console.log(ignore+' points where ignored for being out of bounds');
+      console.log(msg);
+    }
+  }
+
+  return points;
+}//----------------------------------------------------------------------------
